189b506270f002a17dbf0043158c4e3833584e1f
TolyaLaz <toljik1@mail.ru>
2024-09-24 22:23:58 +0400

    add blog_list, blog_form, blog_confirm_delete

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,6 @@ from blog.models import Blog
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('id', 'title', 'content', 'is_published', 'views_count', 'created_at',)
+    list_filter = ('title', 'is_published', 'created_at',)
+    search_fields = ('title', 'content',)
--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,8 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='blog/', verbose_name='Изображение')
     views_count = models.IntegerField(default=0, verbose_name='Просмотры')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    slug = models.CharField(max_length=150, verbose_name='slug', blank=True, null=True)
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
 
     def __str__(self):
         return self.title
